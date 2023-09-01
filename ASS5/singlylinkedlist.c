66758a96fd22e754402d81479b042e018dcce8bd
Arghya Das <tapaskumardas1707@gmail.com>
2023-09-01 19:45:14 +0530

    changes are done

--- ASS5/singlylinkedlist.c
+++ ASS5/singlylinkedlist.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    node *next;
+};
+node *head = NULL;
+
+void push(node **head_ref, int val){
+    node* new_node = (node*)malloc(sizeof(node));
+
+    new_node->data = val;
+    new_node->next = (*head_ref);
+    *head_ref = new_node;
+}
+
+void deleteend(node **head_ref){
+    node *end = *head_ref;
+    node *prev = NULL;
+
+    while(end->next){
+        prev = end;
+        end = end->next;
+    }
+    prev->next = NULL;
+
+    free(end);
+}
